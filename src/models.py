5296214747d4d438ac9e10c1c2c8a580ce36a2f4
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-12-01 01:05:56 +0000

    All Tables have been entered

--- src/models.py
+++ src/models.py
@@ -34,23 +34,81 @@ class Favorite_Characters(Base):
     id = Column(Integer, primary_key=True)
 
     fave_name = Column(String(250), ForeignKey('characters.name'))
-    fave_id = Column(Integer, ForeignKey('characters.id'))
+    fave_id = Column(Integer, ForeignKey('characters.character_id'))
 
 class Favorite_Planets(Base):
     __tablename__ = 'favorite_planets'
     id = Column(Integer, primary_key=True)
 
     fave_name = Column(String(250),  ForeignKey('planets.name'))
-    fave_id = Column(Integer, ForeignKey('planets.id'))
+    fave_id = Column(Integer, ForeignKey('planets.planet_id'))
 
 class Favorite_Vehicles(Base):
-    __tablename__ = 'favorite_vehicless'
+    __tablename__ = 'favorite_vehicles'
     id = Column(Integer, primary_key=True)
 
     fave_name = Column(String(250), ForeignKey('vehicles.name'))
-    fave_id = Column(Integer, ForeignKey('vehicles.id'))
+    fave_id = Column(Integer, ForeignKey('vehicles.vehicle_id'))
 
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    
+    character_id = Column(Integer, unique=True, nullable=False)
+    name = Column(String(250), unique=True, nullable=False)
+    birth_year = Column(String(250), unique=False, nullable=False)
+    gender = Column(String(250), unique=False, nullable=False)
+    height = Column(Integer, unique=False, nullable=False)
+    mass = Column(Integer, unique=False, nullable=False)
+    skin_color = Column(String(250), unique=False, nullable=False)
+    hair_color = Column(String(250), unique=False, nullable=False)
+    eye_color = Column(String(250), unique=False, nullable=True)
+    films = Column(String(250), unique=False, nullable=True)
+    homeworld = Column(String(250), unique=False, nullable=True)
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    
+    planet_id = Column(Integer, unique=True, nullable=False)
+    name = Column(String(250), unique=True, nullable=False)
+    population = Column(Integer, unique=False, nullable=False)
+    residents = Column(String(250), unique=False, nullable=False)
+    terrain = Column(String(250), unique=False, nullable=False)
+    climate = Column(String(250), unique=False, nullable=False)
+    surface_water = Column(Integer, unique=False, nullable=False)
+    diameter = Column(Integer, unique=False, nullable=False)
+    gravity = Column(Integer, unique=False, nullable=False)
+    orbital_period = Column(Integer, unique=False, nullable=False)
+    rotational_period = Column(Integer, unique=False, nullable=False)
+    films = Column(String(250), unique=False, nullable=True)
+    url = Column(String(250), unique=False, nullable=True)
 
-
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    
+    vehicle_id = Column(Integer, unique=True, nullable=False)
+    name = Column(String(250), unique=True, nullable=False)
+    vehicle_class = Column(String(250), unique=False, nullable=False)
+    manufacturer = Column(String(250), unique=False, nullable=False)
+    model = Column(String(250), unique=False, nullable=False)
+    cost_in_credits = Column(Integer, unique=False, nullable=False)
+    crew = Column(Integer, unique=False, nullable=False)
+    pilots = Column(String(250), unique=False, nullable=False)  # Figure out how to make this an array type...
+    passengers = Column(Integer, unique=False, nullable=False)
+    length = Column(Integer, unique=False, nullable=False)
+    cargo_capacity = Column(Integer, unique=False, nullable=False)
+    consumables = Column(String(250), unique=False, nullable=False)
+    max_atmoshering_speed = Column(Integer, unique=False, nullable=False)
+    films = Column(String(250), unique=False, nullable=True)
+    url = Column(String(250), unique=False, nullable=True)
+    
+    
+    
+    
+    
+    
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
