859d26793930ed0460e40657ecbb57669355e0c6
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-12-02 19:05:19 +0000

    Models Complete

--- src/models.py
+++ src/models.py
@@ -17,6 +17,16 @@ class Users(Base):
     phone_number = Column(String(250), unique=False, nullable=False)
     username = Column(String(250), unique=True, nullable=False)
     password = Column(String(80), unique=False, nullable=False)
+    
+    def serialize(self):
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "username": self.username,
+            "password": self.password
+        }
 
 class User_Favorites(Base):
     __tablename__ = 'user_favorites'
@@ -28,6 +38,14 @@ class User_Favorites(Base):
     fave_characters_id = Column(Integer, ForeignKey('favorite_characters.id'))
     fave_planets_id = Column(Integer, ForeignKey('favorite_planets.id'))
     fave_vehicles_id = Column(Integer, ForeignKey('favorite_vehicles.id'))
+    
+    def serialize(self):
+        return {
+            "fave_characters_id": self.fave_characters_id,
+            "fave_planets_id": self.fave_planets_id,
+            "fave_vehicles_id": self.fave_vehicles_id
+        }
+    
 
 class Favorite_Characters(Base):
     __tablename__ = 'favorite_characters'
@@ -35,6 +53,12 @@ class Favorite_Characters(Base):
 
     fave_name = Column(String(250), ForeignKey('characters.name'))
     fave_id = Column(Integer, ForeignKey('characters.character_id'))
+    
+    def serialize(self):
+        return {
+            "fave_name": self.fave_name,
+            "fave_id": self.fave_id
+        }
 
 class Favorite_Planets(Base):
     __tablename__ = 'favorite_planets'
@@ -42,6 +66,12 @@ class Favorite_Planets(Base):
 
     fave_name = Column(String(250),  ForeignKey('planets.name'))
     fave_id = Column(Integer, ForeignKey('planets.planet_id'))
+    
+    def serialize(self):
+        return {
+            "fave_name": self.fave_name,
+            "fave_id": self.fave_id
+        }
 
 class Favorite_Vehicles(Base):
     __tablename__ = 'favorite_vehicles'
@@ -49,6 +79,12 @@ class Favorite_Vehicles(Base):
 
     fave_name = Column(String(250), ForeignKey('vehicles.name'))
     fave_id = Column(Integer, ForeignKey('vehicles.vehicle_id'))
+    
+    def serialize(self):
+        return {
+            "fave_name": self.fave_name,
+            "fave_id": self.fave_id
+        }
 
 class Characters(Base):
     __tablename__ = 'characters'
@@ -65,6 +101,21 @@ class Characters(Base):
     eye_color = Column(String(250), unique=False, nullable=True)
     films = Column(String(250), unique=False, nullable=True)
     homeworld = Column(String(250), unique=False, nullable=True)
+    
+    def serialize(self):
+        return {
+            "character_id": self.character_id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "mass": self.mass,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "films": self.films,
+            "homeworld": self.homeworld
+        }
     
 class Planets(Base):
     __tablename__ = 'planets'
@@ -83,6 +134,23 @@ class Planets(Base):
     rotational_period = Column(Integer, unique=False, nullable=False)
     films = Column(String(250), unique=False, nullable=True)
     url = Column(String(250), unique=False, nullable=True)
+    
+    def serialize(self):
+        return {
+            "planet_id": self.planet_id,
+            "name": self.name,
+            "population": self.population,
+            "residents": self.residents,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            "surface_water": self.surface_water,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "orbital_period": self.orbital_period,
+            "rotational_period": self.rotational_period,
+            "films": self.films,
+            "url": self.url
+        }
 
 class Vehicles(Base):
     __tablename__ = 'vehicles'
@@ -100,10 +168,28 @@ class Vehicles(Base):
     length = Column(Integer, unique=False, nullable=False)
     cargo_capacity = Column(Integer, unique=False, nullable=False)
     consumables = Column(String(250), unique=False, nullable=False)
-    max_atmoshering_speed = Column(Integer, unique=False, nullable=False)
+    max_atmosphering_speed = Column(Integer, unique=False, nullable=False)
     films = Column(String(250), unique=False, nullable=True)
     url = Column(String(250), unique=False, nullable=True)
     
+    def serialize(self):
+        return {
+            "vehicle_id": self.vehicle_id,
+            "name": self.name,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "model": self.model,
+            "cost_in_credits": self.cost_in_credits,
+            "crew": self.crew,
+            "pilots": self.pilots,
+            "passengers": self.passengers,
+            "length": self.length,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "films": self.films,
+            "url": self.url
+        }
     
     
     
